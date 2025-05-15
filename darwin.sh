c300bbfdc300fb3e18bd42dfe890b1eaab164d4e
Zheng Jie <eynsfordcq@gmail.com>
2025-05-15 22:07:22 +0800

    add tailscale alias for mac

--- darwin.sh
+++ darwin.sh
@@ -4,7 +4,12 @@ BREW_PREFIX="/opt/homebrew"
 # aliases
 alias rm='trash'
 
+# tailscale
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
 # functions
 mkcd() {
     mkdir $1 ; cd $1
 }
+
+
